9bc668293619ebe0252c4251a55e3d7544f8f737
Мизерный Максим <maksimmizernyj9@gmail.com>
2026-05-26 22:18:59 +0300

    create file response

---
+++ Server/FitnessTracker.Core/DTO/Files/FileResponse.cs
@@ -0,0 +1,10 @@
+namespace FitnessTracker.Core.DTO;
+
+public class FileResponse
+{
+    public Guid Id {get; set;}
+    public string FileName {get; set;} = string.Empty;
+    public string ContentType {get; set;} = string.Empty;
+    public long Size {get; set;}
+    public DateTime UploadedAt {get; set;}
+}
